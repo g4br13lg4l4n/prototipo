fc66a52f694e63f9bee4ca39ad40dfe43e5b788a
Gabriel Galan <gabriel.galan92@gmail.com>
2019-02-10 01:40:12 -0600

    add seed user

--- database/migrations/2019_01_29_000002_create_users_table.php
+++ database/migrations/2019_01_29_000002_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email', 25)->unique();
-            $table->string('password',150);
-            $table->char('status',12);
+            $table->string('email', 40)->unique();
+            $table->string('password', 150);
+            $table->char('status', 12);
             $table->rememberToken();
             $table->timestamps();
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $cantidadUsuarios = 10;
+        factory(User::class, $cantidadUsuarios)->create();
     }
 }
